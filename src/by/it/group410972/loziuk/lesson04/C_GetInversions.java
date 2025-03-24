aa5e9ab9c4e2257e7d0680e24afa68e2c56839ce
Vitaly Loziuk <vitalik050405@gmail.com>
2025-03-24 22:16:50 +0300

    add lesson 4 tasks

---
+++ src/by/it/group410972/loziuk/lesson04/C_GetInversions.java
@@ -0,0 +1,112 @@
+package by.it.group410972.loziuk.lesson04;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.Scanner;
+
+/*
+Рассчитать число инверсий одномерного массива.
+Сложность алгоритма должна быть не хуже, чем O(n log n)
+
+Первая строка содержит число 1<=n<=10000,
+вторая - массив A[1…n], содержащий натуральные числа, не превосходящие 10E9.
+Необходимо посчитать число пар индексов 1<=i<j<n, для которых A[i]>A[j].
+
+    (Такая пара элементов называется инверсией массива.
+    Количество инверсий в массиве является в некотором смысле
+    его мерой неупорядоченности: например, в упорядоченном по неубыванию
+    массиве инверсий нет вообще, а в массиве, упорядоченном по убыванию,
+    инверсию образуют каждые (т.е. любые) два элемента.
+    )
+
+Sample Input:
+5
+2 3 9 2 9
+Sample Output:
+2
+
+Головоломка (т.е. не обязательно).
+Попробуйте обеспечить скорость лучше, чем O(n log n) за счет многопоточности.
+Докажите рост производительности замерами времени.
+Большой тестовый массив можно прочитать свой или сгенерировать его программно.
+*/
+
+
+public class C_GetInversions {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        InputStream stream = C_GetInversions.class.getResourceAsStream("dataC.txt");
+        C_GetInversions instance = new C_GetInversions();
+        //long startTime = System.currentTimeMillis();
+        int result = instance.calc(stream);
+        //long finishTime = System.currentTimeMillis();
+        System.out.print(result);
+    }
+
+    int calc(InputStream stream) throws FileNotFoundException {
+        //подготовка к чтению данных
+        Scanner scanner = new Scanner(stream);
+        //!!!!!!!!!!!!!!!!!!!!!!!!!     НАЧАЛО ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!
+        //размер массива
+        int n = scanner.nextInt();
+        //сам массив
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = scanner.nextInt();
+        }
+        int result = 0;
+        //!!!!!!!!!!!!!!!!!!!!!!!!     тут ваше решение   !!!!!!!!!!!!!!!!!!!!!!!!
+
+        int[] temp = new int[n];
+
+        result = mergeSortWithCount(a, temp, 0, n - 1);
+
+        //!!!!!!!!!!!!!!!!!!!!!!!!!     КОНЕЦ ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
+        return result;
+    }
+
+    private int mergeSortWithCount(int[] arr, int[] temp, int left, int right) {
+        if (left >= right) {
+            return 0;
+        }
+
+        int middle = left + (right - left) / 2;
+        int count = 0;
+
+        count += mergeSortWithCount(arr, temp, left, middle);
+        count += mergeSortWithCount(arr, temp, middle + 1, right);
+        count += mergeWithCount(arr, temp, left, middle, right);
+
+        return count;
+    }
+
+    private int mergeWithCount(int[] array, int[] temp, int left, int middle, int right) {
+        int i = left;
+        int j = middle + 1;
+        int k = left;
+        int count = 0;
+
+        while (i <= middle && j <= right) {
+            if (array[i] <= array[j]) {
+                temp[k++] = array[i++];
+            } else {
+                temp[k++] = array[j++];
+                count += (middle - i + 1);
+            }
+        }
+
+        while (i <= middle) {
+            temp[k++] = array[i++];
+        }
+
+        while (j <= right) {
+            temp[k++] = array[j++];
+        }
+
+        for (i = left; i <= right; i++) {
+            array[i] = temp[i];
+        }
+
+        return count;
+    }
+}
